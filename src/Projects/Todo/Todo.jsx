1498e3bdb0bcac4e3da26bcfc675e451b914b83c
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-01 14:03:05 +0600

    feat: add react-hot-toast for task notifications in Todo component

--- src/Projects/Todo/Todo.jsx
+++ src/Projects/Todo/Todo.jsx
@@ -1,14 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 const Todo = () => {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const handleInputValue = (value) => {
+    setInputValue(value);
+  };
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (!inputValue) {
+      toast.error("Task cannot be empty!");
+      return;
+    }
+    if (inputValue) {
+      const taskValue = tasks.find((task) => task == inputValue);
+      if (taskValue) {
+        toast.error("Task already exists!");
+        return;
+      }
+    }
+    if (inputValue) {
+      setTasks([...tasks, inputValue]);
+      setInputValue("");
+      toast.success("Task added successfully!");
+    }
+  };
+  console.log(tasks);
   return (
     <div className="py-20 max-w-96 mx-auto">
       <h2 className="text-center text-2xl font-semibold">Todo App</h2>
       <div>
-        <form className="text-center mt-4">
+        <form onSubmit={handleFormSubmit} className="text-center mt-4">
           <input
             type="text"
             placeholder="Add a new task"
+            value={inputValue}
+            onChange={(event) => handleInputValue(event.target.value)}
             className="border border-[#1E293B] rounded-l-md  py-1 px-2"
           />
           <button
@@ -19,6 +46,7 @@ const Todo = () => {
           </button>
         </form>
       </div>
+      <Toaster />
     </div>
   );
 };
